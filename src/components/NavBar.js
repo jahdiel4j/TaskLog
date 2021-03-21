64021755afe857d2e8e77aa7a733e9d296a69fee
AlexLiou <alexliou0@gmail.com>
2021-03-21 14:43:27 -0400

    changed color of tabs

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,11 +6,11 @@ export default class Navbar extends Component {
     render() {
         return (
             <ul class="nav nav-tabs justify-content-center nav-fill">
-                <li class="nav-item">
-                    <Link class="nav-link active" aria-current="page" to="/">Task Log</Link>
+                <li class="nav-item" role="presentation">
+                    <Link class="nav-link border-3 bg-info" aria-current="page" data-bs-toggle="tab" to="/">Task Log</Link>
                 </li>
-                <li class="nav-item">
-                    <Link class="nav-link active" aria-current="page" to="/report">Report</Link>
+                <li class="nav-item" role="presentation">
+                    <Link class="nav-link border-3 bg-info" aria-current="page" data-bs-toggle="tab" to="/report">Report</Link>
                 </li>
             </ul>
         );
