61b35a2873141161bb52205575d8a5e9a972caa0
jahdi <jahdiel4j@gmail.com>
2021-03-22 16:02:29 -0400

    figured out how to change color of navbar buttons to any custom color

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,13 +8,13 @@ export default class Navbar extends Component {
         return (
             <ul class="nav nav-tabs justify-content-center nav-fill">
                 <li class="nav-item" role="presentation">
-                    <Link class="nav-link border-3 bg-warning" aria-current="page" data-bs-toggle="tab" to="/">Title</Link>
+                    <Link class="nav-link border-3" aria-current="page" data-bs-toggle="tab" to="/">Title</Link>
                 </li>
                 <li class="nav-item" role="presentation">
-                    <Link class="nav-link border-3 bg-warning" aria-current="page" data-bs-toggle="tab" to="/task">Task Log</Link>
+                    <Link class="nav-link border-3" aria-current="page" data-bs-toggle="tab" to="/task">Task Log</Link>
                 </li>
                 <li class="nav-item" role="presentation">
-                    <Link class="nav-link border-3 bg-warning" aria-current="page" data-bs-toggle="tab" to="/report">Report</Link>
+                    <Link class="nav-link border-3" aria-current="page" data-bs-toggle="tab" to="/report">Report</Link>
                 </li>
             </ul>
         );
